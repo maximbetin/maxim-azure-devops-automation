2df96fc0643263703cc18a2b88f2e1222a2858c8
Maxim Betin <betinmaxim@gmail.com>
2024-02-12 22:11:24 +0100

    Set the IPs to static

--- terraform/modules/vm/main.tf
+++ terraform/modules/vm/main.tf
@@ -9,7 +9,7 @@ resource "azurerm_virtual_network" "vnet" {
   name                = "${var.prefix}-vnet"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  address_space       = ["10.0.0.0/16"] # TODO: Explain CIDR
+  address_space       = ["10.0.0.0/16"]
 }
 
 # Subnet Configuration
@@ -17,9 +17,18 @@ resource "azurerm_subnet" "subnet" {
     name                 = "${var.prefix}-subnet"
     resource_group_name  = azurerm_resource_group.rg.name
     virtual_network_name = azurerm_virtual_network.vnet.name
-    address_prefixes     = ["10.0.1.0/24"] # TODO: Explain CIDR
+    address_prefixes     = ["10.0.1.0/24"]
   
 }
+
+# Public IP Configuration
+resource "azurerm_public_ip" "public_ip" {
+  name                = "${var.prefix}-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+}
+
 
 # Network Interface Configuration
 resource "azurerm_network_interface" "interface" {
@@ -30,7 +39,8 @@ resource "azurerm_network_interface" "interface" {
   ip_configuration {
     name                          = "${var.prefix}-ip-config"
     subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
+    private_ip_address_allocation = "Static"
+    public_ip_address_id = azurerm_public_ip.public_ip.id
   }
 }
 
